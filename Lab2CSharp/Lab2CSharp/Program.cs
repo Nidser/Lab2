b51dd8dbda930945dd45f137123fa9bcf3211ce6
nidser <johndiezdaly@hotmail.com>
2014-09-24 12:32:47 +0100

    LAb2 - Test Commit

---
+++ Lab2CSharp/Lab2CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = " This is Git hub upload";
+
+            Console.Write(s);
+
+            Console.ReadLine();
+        }
+    }
+}
